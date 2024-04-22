884bf983db9667dcdaed5aa5226963d7ba243b0d
jiteshsoni <jiteshsoni>
2024-04-22 20:11:42 +0000

    delete a file

--- reusable_functions.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import DataFrame
-from pyspark.sql.functions import current_timestamp, current_date
-
-def append_ingestion_columns(_df: DataFrame):
-    return _df.withColumn("ingestion_timestamp", current_timestamp()).withColumn("ingestion_date", current_date())
