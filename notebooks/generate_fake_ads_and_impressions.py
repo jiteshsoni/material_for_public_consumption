ef6e6545a85456963db49478dea1b0bb90b816a4
get2jitesh@gmail.com <get2jitesh@gmail.com>
2024-05-08 18:54:13 +0000

    Test push

--- notebooks/generate_fake_ads_and_impressions.py
+++ notebooks/generate_fake_ads_and_impressions.py
@@ -3,21 +3,37 @@
 
 # COMMAND ----------
 
-import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import current_timestamp
+import uuid
+import datetime
 from faker import Faker
 import random
+import time, json
 from datetime import timedelta
-from pyarrow import parquet as pq
-from pyarrow import Table
-import uuid
 
-# Initialize Faker
+# COMMAND ----------
+
+spark.conf.set("spark.databricks.io.directoryCommit.createSuccessFile","false")
+spark.conf.set("parquet.enable.summary-metadata", "false")
+sc._jsc.hadoopConfiguration().set("mapreduce.fileoutputcommitter.marksuccessfuljobs", "false")
+spark.conf.set("spark.sql.sources.commitProtocolClass","org.apache.spark.sql.execution.datasources.SQLHadoopMapReduceCommitProtocol")
+
+# COMMAND ----------
+
+# Initialize Faker and Spark session
 fake = Faker()
 
 # COMMAND ----------
 
 
-def generate_data(num_entries):
+# Set the prefix path where the files will be stored
+prefix_path = "abfss://storage-container@databrickstrainingadls.dfs.core.windows.net/fake_data/"
+
+# COMMAND ----------
+
+
+def generate_data(num_entries, output_dir):
     impressions = []
     clicks = []
     for _ in range(num_entries):
@@ -35,25 +51,18 @@ def generate_data(num_entries):
         referrer_url = fake.url()
         ad_size = random.choice(['300x250', '728x90', '160x600', '320x50'])
         connection_type = random.choice(['WiFi', '4G', '5G'])
-        
-        # Create impression
+
         impressions.append({
-            "Event_ID": event_id,
             "impression_id": impression_id,
             "User_ID": user_id,
             "Timestamp": timestamp,
             "Ad_Content": ad_content,
-            "Device_Type": device_type,
-            "Operating_System": operating_system,
-            "Browser": browser,
-            "Impression_Location": impression_location,
             "Time_of_Day": time_of_day,
             "Referrer_URL": referrer_url,
             "Ad_Size": ad_size,
             "Connection_Type": connection_type
         })
-        
-        # Randomly decide if this impression leads to a click
+
         if random.random() < 0.1:  # 10% chance of click
             click_timestamp = timestamp + timedelta(seconds=random.randint(1, 3600))
             clicks.append({
@@ -63,40 +72,51 @@ def generate_data(num_entries):
                 "Country": fake.country(),
                 "City": fake.city(),
                 "Latitude": fake.latitude(),
-                "Longitude": fake.longitude()
+                "Longitude": fake.longitude(),
+                "Device_Type": device_type,
+                "Operating_System": operating_system,
+                "Browser": browser,
+                "Impression_Location": impression_location,
             })
-    
-    return pd.DataFrame(impressions), pd.DataFrame(clicks)
 
-# Generate datasets
-impressions_df, clicks_df = generate_data(1000)
+    impressions_df = spark.createDataFrame(impressions)
+    clicks_df = spark.createDataFrame(clicks)
 
-# Example: Display the first few rows of the impressions dataset
-print("Impressions Dataset Sample:")
-print(impressions_df.head())
+    # Generate a single output file for each DataFrame
+    impressions_df.coalesce(1).write.format('json').mode('append').save(output_dir + '/impressions')
+    clicks_df.coalesce(1).write.format('json').mode('append').save(output_dir + '/clicks')
 
-
-# Generate a random UUID for the impressions file name
-impressions_file_name = str(uuid.uuid4()) + '.json'
-# Write the impressions DataFrame to a JSON file with the random UUID as its name
-impressions_df.to_json(impressions_file_name, orient='records', lines=True)
-
-# Generate a random UUID for the clicks file name
-clicks_file_name = str(uuid.uuid4()) + '.json'
-# Write the clicks DataFrame to a JSON file with the random UUID as its name
-clicks_df.to_json(clicks_file_name, orient='records', lines=True)
-
-# Print the generated file names
-print(f"Impressions file: {impressions_file_name}")
-print(f"Clicks file: {clicks_file_name}")
 
 # COMMAND ----------
 
-impressions_df.head()
+while True:
+    start_time = time.time()  # Record the start time
+    generate_data(num_entries=1000, output_dir=prefix_path)
+    end_time = time.time()  # Record the end time after the function has executed
+    elapsed_time = end_time - start_time  # Calculate the time taken to run the function
+    time_to_sleep = max(60 - elapsed_time, 0)  # Ensure the sleep time never goes negative
+    print(f"time_to_sleep: {time_to_sleep}")
+    time.sleep(time_to_sleep)  # Sleep for the remainder of the minute
 
 # COMMAND ----------
 
-clicks_df.head()
+# clicks_df = spark.readStream \
+#     .format("cloudFiles") \
+#     .option("cloudFiles.format", "json") \
+#     .option("cloudFiles.schemaLocation", "/tmp/jite/") \
+#             .option("inferSchema", "true") \
+#     .load("abfss://storage-container@databrickstrainingadls.dfs.core.windows.net/fake_data/clicks/")
+# display(clicks_df)
+
+# COMMAND ----------
+
+# impressions_df = spark.readStream \
+#     .format("cloudFiles") \
+#     .option("cloudFiles.format", "json") \
+#     .option("cloudFiles.schemaLocation", "/tmp/jite2/") \
+#             .option("inferSchema", "true") \
+#     .load("abfss://storage-container@databrickstrainingadls.dfs.core.windows.net/fake_data/impressions/")
+# display(impressions_df)
 
 # COMMAND ----------
 
